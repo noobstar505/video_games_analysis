8c79588c75461fffdc6847e78414710fad41bd1f
sewoon kim <sewoonk1013@gmail.com>
2024-06-04 15:48:01 +0000

    s

---
+++ models/mart/sales_by_country_by_critic_score.sql
@@ -0,0 +1,19 @@
+SELECT SUM(jp_sales) AS jp_sales, SUM(na_sales) AS na_sales,
+ SUM(emea_sales) AS emea_sales, SUM(other_sales) AS other_sales,
+ SUM(total_sales) AS total_sales
+,CASE
+
+  WHEN critic_score >= 0 AND critic_score <=1 THEN "1"
+  WHEN critic_score >= 1 AND critic_score <=2 THEN "2"
+  WHEN critic_score >= 2 AND critic_score <=3 THEN "3"
+  WHEN critic_score >= 3 AND critic_score <=4 THEN "4"
+  WHEN critic_score >= 4 AND critic_score <=5 THEN "5"
+  WHEN critic_score >= 5 AND critic_score <=6 THEN "6"
+  WHEN critic_score >= 6 AND critic_score <=7 THEN "7"
+  WHEN critic_score >= 7 AND critic_score <=8 THEN "8"
+  WHEN critic_score >= 8 AND critic_score <=9 THEN "9"
+  WHEN critic_score >= 9 AND critic_score <=10 THEN "10"
+  ELSE "no_critic_score"
+END AS critic_review_score
+FROM  {{ ref('stg_sales_data__sales') }}
+GROUP BY critic_review_score 
