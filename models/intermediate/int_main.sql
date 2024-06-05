2e797b7824b3af68d9de155d816b6907c247ae12
Tahmid Rahim <tahmidrahim000@gmail.com>
2024-06-05 15:44:46 +0000

    Added a new table

--- models/intermediate/int_main.sql
+++ models/intermediate/int_main.sql
@@ -3,3 +3,4 @@ franchise.name AS franchise_name
 FROM {{ ref('int_companies_main') }} main
 LEFT JOIN {{ ref('stg_igdb__franchise') }} franchise
 ON franchise.id = main.franchise
+WHERE main.release_date IS NOT NULL
